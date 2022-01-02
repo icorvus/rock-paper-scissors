6dde612a57d0fd9a29c7b058f2688457bd8c1f0f
icorvus <business.wrona@gmail.com>
2022-01-02 23:54:56 +0100

    Implemented getRoundOutcome

--- script.js
+++ script.js
@@ -7,3 +7,42 @@ function computerPlay() {
   // and uses it to choose the final result
   return possibleResults[Math.floor(Math.random() * possibleResults.length)];
 }
+
+function getRoundOutcome(playerSelection, computerSelection) {
+  // Selects the winner in a single round of the game
+
+  // Makes function inputs case-insensitive to prevent bugs
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+
+  if (playerSelection === computerSelection) {
+    return `It's a tie! You both chose ${playerSelection}`;
+  } else if (playerSelection === "rock") {
+    if (computerSelection === "paper"){
+      return `You lose! ${computerSelection} beats ${playerSelection}`;
+    } else if (computerSelection === "scissors") {
+      return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+      throw "Unknown error";
+    }
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "scissors") {
+      return `You lose! ${computerSelection} beats ${playerSelection}`;
+    } else if (computerSelection === "rock") {
+      return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+      throw "Unknown error";
+    }
+  } else if (playerSelection === "scissors") {
+    if (computerSelection === "rock") {
+      return `You lose! ${computerSelection} beats ${playerSelection}`;
+    } else if (computerSelection === "paper") {
+      return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+      throw "Unknown error";
+    }
+  } else {
+    throw "Unknown error";
+  }
+}
+
